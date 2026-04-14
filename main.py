00897f4b314326bd2a3b0b2a48053c07493c9063
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-04-14 09:51:56 -0300

    main changes

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import arcade
 
 from src.views import MenuView
-from src.utils import Config, ConfigKey
+from src.utils import Config, ConfigKey, Lang, Language
 
+config = Config()
+lang = Lang(default_language=Language.SPANISH)
+
+window = arcade.Window(
+    Config.get(ConfigKey.WINDOW_WIDTH, 800),
+    Config.get(ConfigKey.WINDOW_HEIGHT, 600),
+    Config.get(ConfigKey.TITLE, "My Game")
+)
 
 def main():
-    """Main function"""
-    config = Config()
-    window = arcade.Window(
-        Config.get(ConfigKey.WINDOW_WIDTH, 800),
-        Config.get(ConfigKey.WINDOW_HEIGHT, 600),
-        Config.get(ConfigKey.TITLE, "My Game")
-    )
+    # Subscribe to config changes to update the window when necessary.    
+    config.field_changed.subscribe(on_config_change)
 
+    # Show the menu.
     menu = MenuView()
-
     window.show_view(menu)
     arcade.run()
+
+
+def on_config_change(key: ConfigKey, value):
+    if key in [ConfigKey.WINDOW_WIDTH, ConfigKey.WINDOW_HEIGHT]:
+        window.set_size(
+            value if key == ConfigKey.WINDOW_WIDTH else Config.get(ConfigKey.WINDOW_WIDTH, 800),
+            value if key == ConfigKey.WINDOW_HEIGHT else Config.get(ConfigKey.WINDOW_HEIGHT, 600)
+        )
+        print(window.size, value)
+    elif key == ConfigKey.TITLE:
+        window.set_caption(Config.get(ConfigKey.TITLE, "My Game"))
 
 
 if __name__ == "__main__":
