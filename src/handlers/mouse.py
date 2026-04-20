d8daf386723d9a49702083d15cfa25d3e6b04a3f
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-04-20 11:16:28 -0300

    mouse events

--- src/handlers/mouse.py
+++ src/handlers/mouse.py
@@ -5,7 +5,14 @@ class MouseHandler:
 	def __init__(self):
 		self.position: tuple[int, int] = (0, 0)
 		self.delta: tuple[int, int] = (0, 0)
+
+		# Events.
 		self.on_mouse_motion = Event[[int, int, int, int]]()
+		self.on_left_click = Event[[int, int]]()
+		self.on_right_click = Event[[int, int]]()
+		self.on_middle_click = Event[[int, int]]()
+		self.on_roll_up = Event[[int, int]]()
+		self.on_roll_down = Event[[int, int]]()
 
 
 	def update_mouse(self, x: int, y: int, dx: int, dy: int):
