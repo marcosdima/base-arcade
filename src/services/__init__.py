f6650a02dce3534a104de6ccb0f8ffae7cc2314b
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-04-29 22:59:30 -0300

    remove leaderboard

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,11 +1,9 @@
 from .config import Config, ConfigKey
 from .lang import Lang, Language
-from .leaderboard import Leaderboard
 
 __all__ = [
     'Lang',
     'Language',
     'Config',
     'ConfigKey',
-    'Leaderboard',
 ]
