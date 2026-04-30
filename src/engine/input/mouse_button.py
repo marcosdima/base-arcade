05b888639af6d027c6f7253b4d2e76bde913015d
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-04-30 20:31:42 -0300

    enum -> intenum

--- src/engine/input/mouse_button.py
+++ src/engine/input/mouse_button.py
@@ -1,9 +1,9 @@
-from enum import Enum
+from enum import IntEnum
 
 import arcade
 
 
-class MouseButton(Enum):
+class MouseButton(IntEnum):
     LEFT = arcade.MOUSE_BUTTON_LEFT
     RIGHT = arcade.MOUSE_BUTTON_RIGHT
     MIDDLE = arcade.MOUSE_BUTTON_MIDDLE
