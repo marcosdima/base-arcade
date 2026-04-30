c18de2512309a25bfb97dbf69fd5e74f57f4ce5d
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-04-29 23:05:43 -0300

    mouse event validation to mouse handler

--- src/engine/input/mouse.py
+++ src/engine/input/mouse.py
@@ -1,4 +1,5 @@
 from ..core import Event
+from .mouse_button import MouseButton
 
 
 class MouseHandler:
@@ -18,3 +19,19 @@ class MouseHandler:
         self.position = (x, y)
         self.delta = (dx, dy)
         self.on_mouse_motion.trigger(x, y, dx, dy)
+
+    def receive_mouse_release(self, x: int, y: int, button: int):
+        if button == MouseButton.LEFT:
+            self.on_left_click.trigger(x, y)
+        elif button == MouseButton.RIGHT:
+            self.on_right_click.trigger(x, y)
+        elif button == MouseButton.MIDDLE:
+            self.on_middle_click.trigger(x, y)
+
+    def receive_mouse_press(self, x: int, y: int, button: int):
+        if button == MouseButton.LEFT:
+            self.on_left_click.trigger(x, y)
+        elif button == MouseButton.RIGHT:
+            self.on_right_click.trigger(x, y)
+        elif button == MouseButton.MIDDLE:
+            self.on_middle_click.trigger(x, y)
--- src/engine/ui/base_view.py
+++ src/engine/ui/base_view.py
@@ -1,14 +1,29 @@
-import arcade
 from arcade import View
 
-from .. import KeyboardHandler, MouseHandler
+from ..input import KeyboardHandler, MouseHandler
+from .ui_element import UIElement, UIElementProps
 
 
 class BaseView(View):
     def __init__(self):
         super().__init__()
+
+        # Input handlers.
         self.keyboard_handler = KeyboardHandler()
         self.mouse_handler = MouseHandler()
+
+        # Root ui element.
+        ui_props = UIElementProps(
+            rect=(0, 0, self.window.width, self.window.height),
+            name='root',
+        )
+        self.root = UIElement(ui_props)
+
+    def on_draw(self):
+        self.root.draw()
+        return super().on_draw()
+
+    """ --- Arcade input event handlers --- """
 
     def on_key_press(self, key: int, modifiers: int):
         # Called when a keyboard key is pressed.
@@ -19,17 +34,18 @@ class BaseView(View):
         self.keyboard_handler.key_released(key)
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
-        # Called when a mouse button is pressed.
-        if button == arcade.MOUSE_BUTTON_LEFT:  # Left click
-            self.mouse_handler.on_left_click.trigger(x, y)
-        elif button == arcade.MOUSE_BUTTON_RIGHT:  # Right click
-            self.mouse_handler.on_right_click.trigger(x, y)
-        elif button == arcade.MOUSE_BUTTON_MIDDLE:  # Middle click
-            self.mouse_handler.on_middle_click.trigger(x, y)
+        # Send event to mouse handler.
+        self.mouse_handler.receive_mouse_press(x, y, button)
+
+        # Call root to propagate the click to the UI.
+        self.root.receive_mouse_press(x, y, button, modifiers)
 
     def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
-        # Called when a mouse button is released.
-        pass
+        # Send event to mouse handler.
+        self.mouse_handler.receive_mouse_release(x, y, button)
+
+        # Call root to propagate the click to the UI.
+        self.root.receive_mouse_release(x, y, button, modifiers)
 
     def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
         # Called when the mouse wheel is scrolled.
@@ -38,6 +54,12 @@ class BaseView(View):
         elif scroll_y < 0:  # Scroll down
             self.mouse_handler.on_roll_down.trigger(x, y)
 
+        # Call root to propagate the scroll to the UI.
+        self.root.receive_mouse_scroll(x, y, scroll_x, scroll_y)
+
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
         # Called when the mouse moves.
         self.mouse_handler.update_mouse(x, y, dx, dy)
+
+        # Call root to propagate the motion to the UI.
+        self.root.receive_mouse_motion(x, y, dx, dy)
