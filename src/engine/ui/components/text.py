f294aabd7c63a395f2281f9bf9ba79189147f89a
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-05-06 23:06:58 -0300

    text_size and text colocation

--- src/engine/ui/components/text.py
+++ src/engine/ui/components/text.py
@@ -1,6 +1,12 @@
+from typing import Literal
+
 import arcade
 
+from ..geometry import Point
 from ..ui_element import UIElement, UIElementProps
+
+AnchorY = Literal['center', 'top', 'bottom']
+AnchorX = Literal['center', 'left', 'right']
 
 
 class TextProps:
@@ -21,6 +27,7 @@ class Text(UIElement):
         props: UIElementProps,
         text_props: TextProps,
     ):
+        props.debug = True
         super().__init__(props)
 
         self.content = text_props.text
@@ -33,20 +40,50 @@ class Text(UIElement):
         self.__current_font = None
         self.__text_obj = None
 
-    def __create_text(self) -> arcade.Text:
+    @property
+    def text_size(self) -> Point:
+        if self.__text_obj:
+            return Point(self.__text_obj.content_width, self.__text_obj.content_height)
+        else:
+            # Estimate text size based on font size and character count.
+            avg_char_width = (
+                self.font_size * 0.6
+            )  # Approximate average character width.
+            width = avg_char_width * len(self.content)
+            height = self.font_size
+            return Point(width, height)
+
+    def __create_text(
+        self, anchor_x: AnchorX = 'center', anchor_y: AnchorY = 'center'
+    ) -> arcade.Text:
         font = self.style.font
         if not font:
             raise ValueError('Font style must be defined for Text element.')
 
+        if anchor_x == 'center':
+            x = self.global_rect.center_x
+        elif anchor_x == 'right':
+            x = self.global_rect.right
+        else:
+            x = self.global_rect.left
+
+        if anchor_y == 'center':
+            y = self.global_rect.center_y
+        elif anchor_y == 'top':
+            y = self.global_rect.top
+        else:
+            y = self.global_rect.bottom
+
         text_obj = arcade.Text(
             self.content,
-            self.rect.x,
-            self.rect.y,
+            x,
+            y,
             font.color.as_tuple(),
             font.size,
-            anchor_x='left',
-            anchor_y='bottom',
+            anchor_x=anchor_x,
+            anchor_y=anchor_y,
         )
+
         self.__current_font = font
         return text_obj
 
@@ -55,14 +92,17 @@ class Text(UIElement):
         if not font:
             return
 
-        if not self.__current_font or font != self.__current_font:
+        if (
+            not self.__current_font
+            or font != self.__current_font
+            or self.__text_obj.text != self.content
+        ):
             self.__text_obj = self.__create_text()
-        else:
-            self.__text_obj.text = self.content
-            self.__text_obj.x = self.rect.x
-            self.__text_obj.y = self.rect.y
 
     def draw(self):
-        self.__update_text()
+        super().draw()
         self.__text_obj.draw()
-        return super().draw()
+
+    def update(self, dt):
+        self.__update_text()
+        super().update(dt)
